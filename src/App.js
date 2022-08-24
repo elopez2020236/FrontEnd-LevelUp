e7e2682d179ea8b7c5a5d9763a8459421a32941f
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-08-24 17:40:04 -0600

    Mejoras

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./App.css";
 import Formulario from "./components/Formulario";
 import Reporte from "./components/reporte";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Formulario />}  />
         <Route path="/reportes" element={<Reporte />}  />
-
       </Routes>
-    </BrowserRouter>
   );
 }
 export default App;
